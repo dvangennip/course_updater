193fbe235b20b3f3c79749e1439ceb56453e6bd5
Doménique van Gennip <vangennip@gmail.com>
2021-09-24 10:28:29 +1000

    Cleanup

--- get_zids.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open('engg1000-engineering-design---2021-t1.csv') as fs:
-	filereader = csv.DictReader(fs)
-
-	print('Username')
-	for row in filereader:
-		print(row['Username'])
